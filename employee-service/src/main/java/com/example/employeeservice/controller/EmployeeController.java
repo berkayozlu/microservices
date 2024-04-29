ebb8efcfd1de8ff27ffd23b89a211bffff39eac2
berkayozlu <berkay1ozlu@gmail.com>
2024-04-29 12:48:21 +0300

    init

--- employee-service/src/main/java/com/example/employeeservice/controller/EmployeeController.java
+++ employee-service/src/main/java/com/example/employeeservice/controller/EmployeeController.java
@@ -4,7 +4,6 @@ import com.example.employeeservice.model.Employee;
 import com.example.employeeservice.repository.EmployeeRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
