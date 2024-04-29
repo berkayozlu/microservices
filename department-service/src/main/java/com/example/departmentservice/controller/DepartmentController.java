90ddd03f5058e5c61fc5a6a70fe8e5181328bf8d
berkayozlu <berkay1ozlu@gmail.com>
2024-04-29 14:37:32 +0300

    init

--- department-service/src/main/java/com/example/departmentservice/controller/DepartmentController.java
+++ department-service/src/main/java/com/example/departmentservice/controller/DepartmentController.java
@@ -6,7 +6,6 @@ import com.example.departmentservice.model.Employee;
 import com.example.departmentservice.repository.DepartmentRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
