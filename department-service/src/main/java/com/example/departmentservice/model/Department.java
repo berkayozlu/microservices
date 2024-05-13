991a73d3b9cb0dfd458ef5332dfe1c043b81a2eb
berkayozlu <berkay1ozlu@gmail.com>
2024-05-13 17:05:33 +0300

    updated

--- department-service/src/main/java/com/example/departmentservice/model/Department.java
+++ department-service/src/main/java/com/example/departmentservice/model/Department.java
@@ -1,11 +1,9 @@
 package com.example.departmentservice.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
+@Table(name = "table_name")
 public class Department {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
